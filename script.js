b2158175409bd43be4d98b7c2fc488e5dcdb48bf
israelAPPC <47697072+israelAPPC@users.noreply.github.com>
2024-05-27 16:56:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ document.getElementById('sorteio-btn').addEventListener('click', function() {
         'Corvinal': 'Corvinal.mp3',
         'Lufa Lufa': 'Lufa.mp3'
     };
+   // Garante que a mesma casa não seja escolhida três vezes seguidas
+    do {
+        casaEscolhida = casas[Math.floor(Math.random() * casas.length)];
+    } while (historico.length >= 2 && casaEscolhida === historico[historico.length - 1] && casaEscolhida === historico[historico.length - 2]);
 
-    const casaEscolhida = casas[Math.floor(Math.random() * casas.length)];
+    historico.push(casaEscolhida);
+    if (historico.length > 3) {
+        historico.shift(); // Mantém apenas os últimos 3 sorteios no histórico
+    }
+
     const resultado = document.getElementById('resultado');
     resultado.textContent = casaEscolhida;
 
